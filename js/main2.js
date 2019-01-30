56e5b83344081d80ec5b82841c20ade74249fc03
Richmond Wong <richmondwong@gmail.com>
2019-01-30 20:06:53 +0000

    Commit demo version

--- js/main2.js
+++ js/main2.js
@@ -12,46 +12,9 @@ init: function(){
   this.stage.disableVisibilityChange = true;
 },
 
-render: function(){
-
-  this.game.debug.body(player)
-  this.game.debug.body(player2)
-},
-
 preload: function(){
-  this.game.load.image('background','assets/BG.png');
-  this.game.load.json('level:1', 'assets/level01.json');
-  this.game.load.image('ground', 'assets/ground.png');
-    this.game.load.image('grass:8x1', 'assets/grass_8x1.png');
-    this.game.load.image('grass:6x1', 'assets/grass_6x1.png');
-    this.game.load.image('grass:4x1', 'assets/grass_4x1.png');
-    this.game.load.image('grass:2x1', 'assets/grass_2x1.png');
-    this.game.load.image('grass:1x1', 'assets/grass_1x1.png');
-
-
-  //  this.game.load.json('level:1', 'assets/test1210pm.json');
-  // this.game.load.image('one', 'assets/1.png');
-  //   this.game.load.image('two', 'assets/2.png');
-  //   this.game.load.image('three', 'assets/3.png');
-  //   this.game.load.image('four', 'assets/4.png');
-  //   this.game.load.image('five', 'assets/5.png');
-  //   this.game.load.image('six', 'assets/6.png');
-  //   this.game.load.image('seven', 'assets/7.png');
-  //   this.game.load.image('eight', 'assets/8.png');
-  //   this.game.load.image('nine', 'assets/9.png');
-  //   this.game.load.image('ten', 'assets/10.png');
-  //   this.game.load.image('eleven', 'assets/11.png');
-  //   this.game.load.image('twelve', 'assets/12.png');
-  //   this.game.load.image('thirteen', 'assets/13.png');
-  //   this.game.load.image('fourteen', 'assets/14.png');
-  //   this.game.load.image('fifteen', 'assets/15.png');
-  //   this.game.load.image('sixteen', 'assets/16.png');
-
-
-
-  // this.load.tilemap('map','assets/level1.csv');
-  // this.load.image('tileset','assets/tileset.png');
-
+  this.load.tilemap('map','assets/level1.csv');
+  this.load.image('tileset','assets/tileset.png');
   this.game.load.image('bullet', 'assets/bullet.png')
   this.game.load.image('bullet2', 'assets/bullet.png')
   // this.load.spritesheet('player','assets/player.png',24,26)
@@ -62,17 +25,14 @@ preload: function(){
 },
 
 create: function(){
-  this.game.add.image(0,0, 'background');
-  this.loadLevel(this.game.cache.getJSON('level:1'));
+  this.stage.backgroundColor = '#3A5963';
 
-  // this.stage.backgroundColor = '#3A5963';
+  map = this.add.tilemap('map',64,64);
+  map.addTilesetImage('tileset');
+  layer = map.createLayer(0);
+  layer.resizeWorld();
 
-  // map = this.add.tilemap('map',64,64);
-  // map.addTilesetImage('tileset');
-  // layer = map.createLayer(0);
-  // layer.resizeWorld();
-
-  // map.setCollisionBetween(0,2);
+  map.setCollisionBetween(0,2);
 
   bullet = this.game.add.weapon(10, 'bullet')
   // bullet.fireLimit = 20
@@ -81,20 +41,17 @@ create: function(){
   bullet2 = this.game.add.weapon(10, 'bullet')
 
 
-  player = this.add.sprite(0,0,'player', 9);//position of the player
+  player = this.add.sprite(550,480,'player', 9);//position of the player
   player.anchor.setTo(0.5,0.5);
-  player.scale.setTo(4,4)
-  player.animations.add('walking', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 9, true);
-  player.animations.add('attack', [17,18,19,20,21,22,23, 0],7,true)
-  player.animations.add('attack', [17, 0],7,false)
+  player.scale.setTo(3,3)
+  player.animations.add('walking', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 9, false);
   player.health = 100
   player.maxhealth = 100
 
-  player2 = this.add.sprite(1280,960,'test', 3);//position of the player
+  player2 = this.add.sprite(300,560,'test', 3);//position of the player
   player2.anchor.setTo(0.5,0.5);
   player2.scale.setTo(4,4)
   player2.animations.add('walking2', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 24, false);
-  player2.animations.add('attack2', [17,18,19,20,21,22,23, 0],7,false)
   player2.health = 100
   player2.maxhealth = 100
 
@@ -145,9 +102,6 @@ update: function(){
   this.physics.arcade.overlap(player2, bullet.bullets, this.playerHit, null, this)
   this.physics.arcade.overlap(player, bullet2.bullets, this.playerHit2, null, this)
 
-  // this.physics.arcade.overlap(player, player2, this.playerMelee, null, this)
-  // this.physics.arcade.overlap(player, bullet2.bullets, this.playerHit2, null, this)
-
   this.physics.arcade.collide(player,layer)
   this.physics.arcade.collide(player2,layer)
 
@@ -157,7 +111,6 @@ update: function(){
   player2.body.velocity.x = 0;
   player2.body.velocity.y = 0;
 
-  player.body.setSize(15,15,7, 7)
 
 
   if (cursors.left.isDown){
@@ -192,8 +145,7 @@ update: function(){
     bullet.fire()
    }
    if (meleeButton.isDown){
-    player.play('attack')
-    this.playerMelee(player2)
+
    }
 
 
@@ -229,22 +181,11 @@ handleCollisions: function(){
   this.game.physics.arcade.collide(player, player2)
 },
 
-
-playerMelee: function(enemyPlayer){
-
-  if (this.physics.arcade.collide(player, player2)){
-    enemyPlayer.damage(1)
-  // currentHealthStatus.scale.setTo(player2.health / player2.maxHealth, 1)
-  this.player2AnimatedHealthBar()
-  console.log(enemyPlayer.health)
-  }
-},
-
 playerHit: function(enemyPlayer, bullet){
   bullet.kill()
   // enemyPlayer.kill()
   enemyPlayer.damage(5)
-  this.player2AnimatedHealthBar()
+  currentHealthStatus.scale.setTo(player2.health / player2.maxHealth, 1)
   console.log(enemyPlayer.health)
 
   // if (enemyPlayer.health === 0){
@@ -260,18 +201,6 @@ playerHit2: function(enemyPlayer2, bullet){
   // enemyPlayer.kill()
   enemyPlayer2.damage(5)
   console.log(enemyPlayer2.health)
-},
-
-player2AnimatedHealthBar: function(){
-  currentHealthStatus.scale.setTo(player2.health / player2.maxHealth, 1)
-},
-
-loadLevel: function(data) {
-  data.platforms.forEach(this.spawnPlatform, this);
-},
-
-spawnPlatform: function(platform) {
-    this.game.add.sprite(platform.x, platform.y, platform.image);
 }
 
 
@@ -282,9 +211,7 @@ spawnPlatform: function(platform) {
 
 
 window.onload = function() {
-    // let game = new Phaser.Game(1960, 800, Phaser.AUTO, 'game');
-    let game = new Phaser.Game(1280, 960, Phaser.AUTO, 'game');
-    // ppppp
+    let game = new Phaser.Game(1960, 800, Phaser.AUTO, 'game');
     game.state.add('play', PlayState);
     // game.state.start('play', true, false, {level: 0});
     game.state.start('play');
